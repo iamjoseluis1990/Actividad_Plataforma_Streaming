fd576bcbca6540bea115c613bf2be80daa1ae19d
iamjoseluis1990 <josel.hernandez97@gmail.com>
2026-09-23 12:53:36 +0200

    Init Program.cs

---
+++ Program.cs
@@ -0,0 +1,10 @@
+namespace Actividad_Plataforma_Streaming
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hola");
+        }
+    }
+}
